42a68a18464c35d1c83f1f812f515b1857148d71
santeri79 <dobryninav79@gmail.com>
2024-06-21 16:17:28 +0300

    Lab1.1

---
+++ Lab1.4/lab1.4.py
@@ -0,0 +1,12 @@
+from ipaddress import IPv4Network
+import random
+
+class IPv4RandomNetwork(IPv4Network):
+    def __init__(self):
+        net = random.randint(0X0b000000, 0xdf000000)
+        mask = random.randint(8,24)
+        IPv4Network.__init__(self, (net, mask), strict=False)
+
+net1 = IPv4RandomNetwork()
+print(net1)
+
