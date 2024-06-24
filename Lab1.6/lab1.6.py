786ecaae7d4b424d8ec1059c27ace2d80dcc3180
santeri79 <dobryninav79@gmail.com>
2024-06-24 18:15:14 +0300

    Lab1.1

---
+++ Lab1.6/lab1.6.py
@@ -0,0 +1,36 @@
+import glob
+import re
+import ipaddress
+
+files = glob.glob('*.log')
+print(files)
+
+def match_func(s):
+    m = re.match("^ip address ((?:[0-9]{1,3}[.]?){4}) ((?:[0-9]{1,3}[.]?){4})", s)
+    if m:
+        return(ipaddress.ipv4interface(m.group(1)))
+    else:
+        return None
+
+
+#s = "ip address 192.168.1.1 255.255.255.0"
+#print(f(s))
+
+for i in files:
+    with open(i) as f:
+        line_list = f.readlines()
+        for line in line_list:
+            s1 = match_func(line)
+            if s1:
+                print(s1)
+
+
+
+        f.close()
+
+   # if s.find("ip address") != -1:
+    #    return ipaddress.ip_address(s)
+    #else: return None
+
+
+
