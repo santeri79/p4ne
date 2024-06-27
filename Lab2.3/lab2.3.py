c4a6920333fcdc3d2355ba65b80a63b2cf43073d
santeri79 <dobryninav79@gmail.com>
2024-06-27 16:36:55 +0300

    Lab1.1

--- Lab2.3/lab2.3.py
+++ Lab2.3/lab2.3.py
@@ -1,4 +1,25 @@
 #10.31.70.208
 
 import ansible
+import glob
+import paramiko
+import time
 
+USER= "av.dobrynin"
+PASSWORD = "q1q1q1"
+TIMEOUT = 2
+BUF_SIZE = 100*1024
+
+ssh_connection = paramiko.SSHClient()
+ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ssh_connection.connect("10.31.70.208", username=USER, password=PASSWORD, look_for_keys=False, allow_agent=False)
+session=ssh_connection.invoke_shell()
+
+session.send("ansible-playbook -i inventory.ini --list-hosts set.yml\n")
+
+time.sleep(TIMEOUT)
+
+out = session.recv(BUF_SIZE)
+m = out.decode()
+print(m)
