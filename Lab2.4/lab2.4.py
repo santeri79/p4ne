0cf904a7afed5ace5731b169b8c912a1ef7e3fba
santeri79 <dobryninav79@gmail.com>
2024-06-28 17:54:14 +0300

    Lab1.1

--- Lab2.4/lab2.4.py
+++ Lab2.4/lab2.4.py
@@ -10,7 +10,8 @@ PASSWORD = "j0sg1280-7@"
 headers = {
     "accept" : "application/yang-data+json",
     "Content-Type": "application/yang-data+json"
-}
+          }
+
 url = "https://10.31.70.209/restconf/data/Cisco-IOS-XE-process-memory-oper:memory-usage-processes"
 
 r = requests.get(url, headers=headers, auth=(USER, PASSWORD), verify=False)
@@ -21,14 +22,9 @@ k = p["Cisco-IOS-XE-process-memory-oper:memory-usage-processes"]["memory-usage-p
 
 
 app = Flask(__name__)
+
 def sort_func(s):
     return int(s['allocated-memory'])
-
-    if m :
-        return m.group(1)
-
-    else:
-        return None
 
 @app.route('/')
 @app.route('/index')
@@ -36,7 +32,7 @@ def index():
     return """In order to connect to a server, the client will first have to resolve the servername
               to an IP address - the location on the Internet where the server resides. Thus, in order for your
              web server to be reachable, it is necessary that the servername be in DNS.
-        """
+           """
 @app.route('/process')
 def process():
             return jsonify(n)
