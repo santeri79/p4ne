b21a952d8d517d1571a36196dbcffaa39ee09d96
santeri79 <dobryninav79@gmail.com>
2024-06-28 17:44:46 +0300

    Lab1.1

--- Lab2.4/lab2.4.py
+++ Lab2.4/lab2.4.py
@@ -1,8 +1,6 @@
-import pprint
+
 import requests
-from flask import Flask
-import glob
-import re
+from flask import Flask, jsonify
 
 
 USER= "restapi"
@@ -17,19 +15,14 @@ url = "https://10.31.70.209/restconf/data/Cisco-IOS-XE-process-memory-oper:memor
 
 r = requests.get(url, headers=headers, auth=(USER, PASSWORD), verify=False)
 p = r.json()
-print(type(p))
-pprint.pprint(p)
 
-for key in p:
-   print(f'{key},{p[key]}', file=result.py)
 
-for i in p["Cisco-IOS-XE-process-memory-oper:memory-usage-processes"]["memory-usage-process"]:
-    print(i["name"], ":", i["allocated-memory"])
+k = p["Cisco-IOS-XE-process-memory-oper:memory-usage-processes"]["memory-usage-process"]
 
-######################################################################################################
+
 app = Flask(__name__)
-def match_conf(s):
-    m = re.match("^ip address ((?:[0-9]{1,3}[.]?){4}) ((?:[0-9]{1,3}[.]?){4})", s)
+def sort_func(s):
+    return int(s['allocated-memory'])
 
     if m :
         return m.group(1)
@@ -40,28 +33,20 @@ def match_conf(s):
 @app.route('/')
 @app.route('/index')
 def index():
-    return """In order to connect to a server, the client will first have to resolve the servername 
-              to an IP address - the location on the Internet where the server resides. Thus, in order for your 
-             web server to be reachable, it is necessary that the servername be in DNS.        
+    return """In order to connect to a server, the client will first have to resolve the servername
+              to an IP address - the location on the Internet where the server resides. Thus, in order for your
+             web server to be reachable, it is necessary that the servername be in DNS.
         """
 @app.route('/process')
 def process():
-            return str(p)
+            return jsonify(n)
 
 if __name__ == '__main__':
 
-    n = []
 
-    for i in files:
-        with open(i) as f:
-            line_list = f.readlines()
-            for line in line_list:
-                s1 = match_conf(line.strip())
-                if s1:
-                    n.append(s1)
+    n = sorted(k, key=sort_func,reverse=True)[0:10]
 
-
-            f.close()
 
     app.run(debug=True)
+
 
